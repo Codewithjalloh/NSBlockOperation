122c031c2ed422c2b3d7cf4a5b99826435da385b
info@codewithjalloh.com <supremejalloh>
2016-07-09 10:51:34 +0100

    code complete

--- NSBlockOperation/ViewController.swift
+++ NSBlockOperation/ViewController.swift
@@ -9,11 +9,47 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var googlewebs:NSArray = []
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let queue = NSOperationQueue()
+        let operationOne: NSBlockOperation = NSBlockOperation(block: {self.getWebs()
+            
+            let operationTwo: NSBlockOperation = NSBlockOperation(block: {
+            self.loadWebs()
+                
+            })
+            queue.addOperation(operationTwo)
+        })
+        queue.addOperation(operationOne)
+        
     }
+    
+    
+    
+    func loadWebs() {
+        let urls: NSMutableArray = NSMutableArray (objects:NSURL(string:"http://www.google.es")!, NSURL(string: "http://www.apple.com")!,NSURL(string: "http://carlosbutron.es")!, NSURL(string: "http://www.bing.com")!,NSURL(string: "http://www.yahoo.com")!)
+        urls.addObjectsFromArray(googlewebs as [AnyObject])
+        
+        for iterator: AnyObject in urls {
+            print("Downloaded \(iterator)")
+        }
+        
+    }
+    
+    
+    func getWebs() {
+        let languages:NSArray = ["com", "ad", "ae", "com.af", "com.ag", "com.ai", "am", "co.uk", "as", "at"]
+        let languageWebs = NSMutableArray()
+        for(var i=0;i < languages.count; i++) {
+            let webStr: NSString = "https://www.google.\(languages[i])"
+            languageWebs.addObject(NSURL(fileURLWithPath: webStr as String))
+        }
+        googlewebs = languageWebs
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
